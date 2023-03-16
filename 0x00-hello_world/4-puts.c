292900786fd50906380c75fadd68465595f50d93
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-16 20:52:04 +0300

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * 
+ * Return: 0
+*/
+int main()
+{
+    puts("Programming is like building a multilingual puzzle\n");
+}
