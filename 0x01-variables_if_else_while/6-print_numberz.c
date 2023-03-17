b7ce5c870f98dc4b1367fdf86c85329f249daf8f
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-17 17:29:11 +0300

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
