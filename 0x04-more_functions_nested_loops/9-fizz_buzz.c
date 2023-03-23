3ae95aaa5cae9db6168e7b522d9d7b7ef2bd0e01
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-24 02:49:26 +0300

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100 separated by space,
+ *		followed by a new line. But for multiples of 3 print "Fizz",
+ *		for the multiples of 5 print "Buzz"
+ *		& "FizzBuzz" for multiples of both 3 & 5.
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 5 == 0 && num % 3 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			putchar(' ');
+		else
+			putchar('\n');
+	}
+	return (0);
+}
