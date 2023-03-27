872506ddd4202b316d1a2777ad3e398eaca94196
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-28 00:20:35 +0300

    Task 8 - fix for when n <= 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
+		if (i == n - 1)
+		{
+			printf("%d\n", a[n - 1]);
+			break;
+		}
 		printf("%d, ", a[i]);
 	}
-	printf("%d\n", a[n - 1]);
 }
