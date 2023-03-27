d64102a27fc4b77bd503005105ff1e19abd3c894
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-27 22:32:06 +0300

    Task 4 - Print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a String in reverse followed by a newline.
+ * @s : String to be printed.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	int _strlen(char *s);
+	len = _strlen(s);
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * _strlen - returns length of a String.
+ * @s : String operand.
+ *
+ * Return: Integer length of string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (1)
+	{
+		if (s[len] == 0)
+			break;
+		len++;
+	}
+
+	return (len);
+}
+
