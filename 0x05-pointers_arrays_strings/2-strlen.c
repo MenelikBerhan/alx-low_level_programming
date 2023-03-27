96508b6721eb07921dfe0f345cbcf1933b48ad05
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-27 22:08:18 +0300

    Task 2 - Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a String.
+ * @s : String operand.
+ *
+ * Return: Integer length of string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (1)
+	{
+		if (s[len] == 0)
+			break;
+		len++;
+	}
+
+	return (len);
+}
