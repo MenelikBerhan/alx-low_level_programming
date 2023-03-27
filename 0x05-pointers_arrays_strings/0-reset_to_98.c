fa824c154c0e4aea518642ebe10ab36e12c0cc17
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-27 19:13:25 +0300

    Task 0 - A fun that reset pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the parameter to point to value of '98'.
+ * @n: a pointer to int type.
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Descriptions for `c` files in `0x05-pointers_arrays_strings` directory
