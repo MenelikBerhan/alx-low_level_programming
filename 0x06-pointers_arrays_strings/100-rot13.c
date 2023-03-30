756264ed34e0d3b814d358a9bee37d9e76575680
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-03-30 04:06:30 +0300

    Task 8 - took time to add Break!!

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: pointer to the string operand.
+ *
+ * Return: the string encoded by rot13.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *c_i = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *c_o = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (*(s + i) == c_i[j])
+			{
+				*(s + i) = c_o[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
