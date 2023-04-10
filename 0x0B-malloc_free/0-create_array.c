824029e0857611e043d088c3f93bc0eab70a01a5
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-10 19:00:00 +0300

    Fix 0-create_array.c
    Put the check for mallock return after assignment to arr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = malloc(size);
+
+	if (size == 0 || arr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 
-	if (size == 0 || arr == NULL)
-		return (NULL);
 	return (arr);
 }
