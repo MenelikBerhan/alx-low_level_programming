f8943b051016d75eeddd71e495eb0046658aa600
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-11 04:24:16 +0300

    Fix Task 6
    Used size of each word to allocate memory rather than using max
    character count amongst the words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,34 +11,36 @@
 
 char **strtow(char *str)
 {
-	int j, row = 0, i = 0, *dim, k;
+	int j, k, words, len = 0, row = 0, i = 0;
 	char **ans, *word;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 
-	dim = find_dimn(str);
-	if (dim[1] == 0)
+	for (k = 0; str[k]; k++)
+		len++;
+
+	words = word_count(str, len);
+	if (words == 0)
 		return (NULL);
-	ans = (char **) malloc(sizeof(char *) * (dim[1] + 1));
+	ans = (char **) malloc(sizeof(char *) * (words + 1));
+
 	if (ans == NULL)
 		return (NULL);
 
-	while (i < dim[2])
+	while (i < len)
 	{
 		if (str[i] != 32)
 		{
-
-			for (k = 0; str[i + k] == 32; k++)
+			for (k = 0; (str[i + k] != 32) && (str[i + k] != 0); k++)
 				continue;
-			word = malloc((k + 1) * sizeof(char));
+
+			word = malloc(sizeof(char) * (k + 1));
 			if (word == NULL)
 				return (NULL);
 
 			for (j = 0; str[i + j] != 32 && str[i + j]; j++)
-			{
 				word[j] = str[i + j];
-			}
 
 			word[j] = 0;
 			ans[row] = word;
@@ -48,48 +50,34 @@ char **strtow(char *str)
 		i++;
 	}
 	ans[row] = NULL;
-
 	return (ans);
 }
 
 /**
- * find_dimn - finds the number of words and maximum length of word after
- * string str is splitted into words using space as a delimiter.
+ * word_count - finds the the number of words after string str is
+ * splitted into words using space as a delimiter.
  * @str: the string to be splitted.
+ * @len: length of string str.
  *
- * Return: A pointer to an array of integer of size three with
- * maximum word length as first element,
- * number of words as second element and
- * length of string str as third element.
+ * Return: Number of words.
  */
 
-int *find_dimn(char *str)
+int word_count(char *str, int len)
 {
-	int i, j, len = 0;
-	int word_count = 0, char_count, char_count_max = 0;
-	int *res = malloc(3 * sizeof(int));
+	int i, j, word_count = 0;
 
-	for (i = 0; str[i]; i++)
-		len++;
 	for (i = 0; i < len; i++)
 	{
 		if (str[i] != 32)
 		{
-			j = char_count = 1;
-
+			j = 1;
 			while (str[i + j] != 32 && i + j < len)
-			{
-				char_count++;
 				j++;
-			}
+
 			word_count++;
 			i += j;
-			if (char_count_max < char_count)
-				char_count_max = char_count;
 		}
 	}
-	res[0] = char_count_max;
-	res[1] = word_count;
-	res[2] = len;
-	return (res);
+
+	return (word_count);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,6 +10,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-int *find_dimn(char *str);
+int word_count(char *str, int len);
 
 #endif
