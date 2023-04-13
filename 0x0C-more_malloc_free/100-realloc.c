7c8397c1b9335863ec905a2f0c03c6826ad2d58e
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-13 03:02:02 +0300

    Fix Task 4
    Add conditional for case new_size = old_size to return ptr.

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,6 +26,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size == old_size)
+		return (ptr);
 
 	arr = malloc(new_size);
 	if (arr == NULL)
