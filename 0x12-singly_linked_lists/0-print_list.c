0c89b97822178500de396005f46303382152a397
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-28 03:59:58 +0300

    Add Files Task 0
    Add 0-print_list.c
    Update lists.h - with prototype for print_list function.
    Update README.md - Remove duplicate lines.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of list_t list h.
+ * @h: a list_t list to be printed.
+ *
+ * Return: The number of nodes in list h.
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *list = h;
+	char *str;
+	int i = 0, n;
+
+	while (list)
+	{
+		str = list->str ? list->str : "nil";
+		n = list->str ? list->len : 0;
+
+		printf("[%u] (%s)\n", n, str);
+
+		list = list->next;
+		i++;
+	}
+
+	return (i);
+}
