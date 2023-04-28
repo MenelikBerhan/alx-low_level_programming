d5e20518ec5c649c742ac4b6a5609059214c57a6
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-28 05:55:33 +0300

    Taks 5

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,18 @@
+global _start
 
-bal    main
-          extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, Holberton\n",
+  mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg
