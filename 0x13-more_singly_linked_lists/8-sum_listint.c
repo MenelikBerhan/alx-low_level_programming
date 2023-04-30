f088cd75eecf673752732e2e80f48866ab0cf792
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-05-01 02:06:43 +0300

    Add files for Task 8 & 9
    Add 8-sum_listint.c
    Add 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Calcuates the sum of all the data (n) of
+ * a listint_t linked list.
+ * @head: a listint_t list.
+ *
+ * Return: The sum of data(n), or 0 if the list is empty.
+*/
+int sum_listint(listint_t *head)
+{
+	unsigned int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * insert_nodeint_at_index - Creates a new node with data (n) and inserts it
+ * at idx index. Index starts from 0.
+ * @head: a pointer to a listint_t list.
+ * @idx: index where the new node is to be inserted.
+ * @n: n data field of new element to be created.
+ *
+ * Return: Rhe address of the new node, or NULL if it failed or
+ * if idx is not a valid index and new node can not be added there.
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode, *temp;
+	unsigned int i;
+
+	if (!head || (!(*head) && idx != 0))
+		return (NULL);
+
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = n;
+	temp = *head;
+
+	for (i = 0; temp && temp->next && idx != 0 && (i < idx - 1); i++)
+		temp = temp->next;
+
+	if (i < (idx - 1) && idx != 0)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		newNode->next = temp;
+		*head = newNode;
+	}
+	else
+	{
+		newNode->next = temp->next;
+		temp->next = newNode;
+	}
+
+	return (newNode);
+}
