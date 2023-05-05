0d56ed51fb99c71c7130bf12f9dbeafb4225ae07
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-05-05 03:56:51 +0300

    Add comments to 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * get_endianness - Checks for which type of endianness used on the
  * machine used to run the program.
  *
  * Return: 0 if big endian, 1 if little endian.
  * Description: Big endian - first byte of a number's value is stored 1st,
- * while Little endian - last byte of a number's value is stored 1st.
+ * while Little endian - last byte of a number's value is stored 1st in memory.
 */
 int get_endianness(void)
 {
-	unsigned short int test = 16972; /* in hex = 0x424C , in (char *) = "BL" */
+	/*in hex = 0x424C , in (char *) = "BL" for big or "LB" for little endian.*/
+	unsigned short int test = 16972;
 
 	return (((char *) &test)[0] == 'L');
 	/*  Or: return (((char *) &test)[1] == 'B'); */
