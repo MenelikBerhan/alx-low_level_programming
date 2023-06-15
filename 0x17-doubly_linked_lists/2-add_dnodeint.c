c5618284c29b72c710e0d7323c2a9eaae4dc5ebc
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-16 00:59:16 +0300

    Add assignment of new_node to *head->prev

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,6 +18,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
+	if (*head)
+		(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
 
