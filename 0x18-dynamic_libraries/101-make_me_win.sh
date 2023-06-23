660551a145b4130a52f872e7b86dcbbc361e5518
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-06-24 00:29:50 +0300

    Change wget option and use PWD in LD_PRELOAD value

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O ../libsrand.so https://github.com/MenelikBerhan/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libsrand.so
-export LD_PRELOAD="../libsrand.so"
+wget -P .. https://github.com/MenelikBerhan/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libsrand.so
+export LD_PRELOAD="$PWD/../libsrand.so"
