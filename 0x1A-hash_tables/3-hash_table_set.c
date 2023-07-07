8875e148f8d281a75205a1bc044a45ae93a9e2c3
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-07-07 06:10:33 +0300

    Minor change

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,11 +15,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *temp;
 	unsigned long int index;
 
-	if (!ht || !(ht->array) || !(ht->size) || !key || !value || *(key) == 0)
+	if (!ht || !(ht->array) || !key || !value || *(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *) key, ht->size);
-
+	if (index >= ht->size)
+		return (0);
 	if (!(ht->array[index]))
 	{
 		new_node = malloc(sizeof(hash_node_t));
