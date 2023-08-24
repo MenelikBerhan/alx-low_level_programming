1feb2d17f828adf8f141481980a98a7636bd2082
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-24 04:18:35 +0300

    Add fle for Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+"""Contains a funciton that calculates perimeter"""
+
+
+def island_perimeter(grid):
+    """`int`: Returns the perimeter of the island described in grid"""
+    perimeter = 0
+    in_island = 0
+    for i, row in enumerate(grid):
+        row_cells = row.count(1)
+        if not in_island and row_cells:
+            in_island = 1
+            perimeter += row_cells
+        if row_cells:
+            perimeter += 2
+            first_1 = row.index(1)
+            try:
+                next_0 = row.index(0, first_1)
+            except ValueError:
+                next_0 = first_1 + 1
+
+            try:
+                next_1 = row.index(1, next_0)
+            except ValueError:
+                next_1 = -1
+            if next_1 != -1:
+                try:
+                    last_1 = row.index(1, next_1 + 1)
+                except ValueError:
+                    last_1 = next_1
+            else:
+                last_1 = -1
+            perimeter += next_0 - first_1
+            perimeter += last_1 - next_1
+    return perimeter
