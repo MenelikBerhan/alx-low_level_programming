ebf792749c95343c1534b4c66ca35054686c7c7d
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-24 05:26:41 +0300

    Check for empty or noe grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,6 +8,8 @@ def island_perimeter(grid):
     """
     `int`: Returns the perimeter of the island described in grid
     """
+    if grid is None or len(grid) == 0:
+        return 0
     perimeter = 0
     in_island = 0
     for i, row in enumerate(grid):
@@ -15,22 +17,30 @@ def island_perimeter(grid):
         if not in_island and row_cells:
             in_island = 1
             perimeter += row_cells
+            # print(perimeter)
         if row_cells:
             perimeter += 2
+            # print("below +2", perimeter)
             curr_first = row.index(1)
             try:
                 curr_last = row.index(0, curr_first) - 1
             except ValueError:
                 curr_last = curr_first
             if grid[i + 1].count(1) == 0:
+
                 perimeter += curr_last - curr_first + 1
+                # print(i, "here",perimeter, "curlast", curr_last, "cur
+                # first", curr_first)
             if grid[i - 1].count(1) != 0:
                 prev_first = grid[i - 1].index(1)
+                perimeter += abs(curr_first - prev_first)
                 try:
                     prev_last = grid[i - 1].index(0, prev_first) - 1
                 except ValueError:
                     prev_last = prev_first
-                perimeter += abs(curr_first - prev_first)
                 perimeter += abs(curr_last - prev_last)
+                # print(i, "here",perimeter, "curlast", curr_last,
+                # "cur first", curr_first, "prevfir", prev_first, "prevlas",
+                # prev_last)
 
     return perimeter
