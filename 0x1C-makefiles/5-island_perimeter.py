02e3448aa853995dfd4f808bd89f06951d18dc3e
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-08-24 18:14:28 +0300

    Use change in values at same index to calculate lateral perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,8 +8,6 @@ def island_perimeter(grid):
     """
     `int`: Returns the perimeter of the island described in grid
     """
-    if grid is None or len(grid) == 0:
-        return 0
     perimeter = 0
     in_island = 0
     for i, row in enumerate(grid):
@@ -17,30 +15,16 @@ def island_perimeter(grid):
         if not in_island and row_cells:
             in_island = 1
             perimeter += row_cells
-            # print(perimeter)
-        if row_cells:
+            print("first row", perimeter)
+        if i == len(grid) - 1 or grid[i + 1].count(1) == 0:
+            perimeter += row_cells + 2
+            print("last row", perimeter)
+            break
+        if in_island:
             perimeter += 2
-            # print("below +2", perimeter)
-            curr_first = row.index(1)
-            try:
-                curr_last = row.index(0, curr_first) - 1
-            except ValueError:
-                curr_last = curr_first
-            if grid[i + 1].count(1) == 0:
-
-                perimeter += curr_last - curr_first + 1
-                # print(i, "here",perimeter, "curlast", curr_last, "cur
-                # first", curr_first)
-            if grid[i - 1].count(1) != 0:
-                prev_first = grid[i - 1].index(1)
-                perimeter += abs(curr_first - prev_first)
-                try:
-                    prev_last = grid[i - 1].index(0, prev_first) - 1
-                except ValueError:
-                    prev_last = prev_first
-                perimeter += abs(curr_last - prev_last)
-                # print(i, "here",perimeter, "curlast", curr_last,
-                # "cur first", curr_first, "prevfir", prev_first, "prevlas",
-                # prev_last)
-
+            print("for height + 2", perimeter)
+            for j, num in enumerate(row):
+                if grid[i + 1][j] + num == 1:
+                    perimeter += 1
+            print("for sum", perimeter)
     return perimeter
